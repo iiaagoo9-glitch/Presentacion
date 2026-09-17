14dd4a51d64a9f35d8074c36d91f2f744a85ced0
Iago Varela <iiaagoo9@gmail.com>
2026-09-17 17:56:25 +0200

    Implement Store class with game, customer, and purchase management

--- src/presentacion/Store.java
+++ src/presentacion/Store.java
@@ -1,5 +1,70 @@
 package presentacion;
 
-public class Store {
 
-}
+	import java.util.ArrayList;
+
+	public class Store {
+
+	    private ArrayList<Game> games;
+	    private ArrayList<Customer> customers;
+	    private ArrayList<Purchase> purchases;
+
+	    public Store() {
+	        games = new ArrayList<Game>();
+	        customers = new ArrayList<Customer>();
+	        purchases = new ArrayList<Purchase>();
+	    }
+
+	   
+	    public void addGame(Game Game) {
+
+	        boolean existe = false;
+
+	        for (Game g : games) {
+	            if (g.getId() == Game.getId()) {
+	                existe = true;
+	            }
+	        }
+
+	        if (!existe) {
+	            games.add(Game);
+	        }
+	    }
+
+	   
+	    public Game findGame(int id) {
+
+	        for (Game Game : games) {
+	            if (Game.getId() == id) {
+	                return Game;
+	            }
+	        }
+
+	        return null;
+	    }
+
+	   
+	    public ArrayList<Game> getGames() {
+	        return games;
+	    }
+
+	   
+	    public void addCustomer(Customer customer) {
+	        customers.add(customer);
+	    }
+
+	  
+	    public ArrayList<Customer> getCustomers() {
+	        return customers;
+	    }
+
+	 
+	    public void addPurchase(Purchase purchase) {
+	        purchases.add(purchase);
+	    }
+
+	  
+	    public ArrayList<Purchase> getPurchases() {
+	        return purchases;
+	    }
+	}
